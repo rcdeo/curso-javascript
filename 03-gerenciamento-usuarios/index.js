b3a007dcd16efd5d711654cf2962a30e19807eee
Rafael Cortes <rafael_cortes@outlook.com>
2024-02-22 15:57:50 -0300

    G2 - Manipulando o DOM

---
+++ 03-gerenciamento-usuarios/index.js
@@ -0,0 +1,11 @@
+// @ts-ignore
+let ex1 = document.querySelector('#exampleInputName').outerHTML;
+console.log(ex1);
+
+// @ts-ignore
+let ex2 = (document.querySelector('#exampleInputName').value = 'Sicrano de Tal');
+console.log(ex2);
+
+// @ts-ignore
+let ex3 = document.querySelector('#exampleInputName').value;
+console.log(ex3);
