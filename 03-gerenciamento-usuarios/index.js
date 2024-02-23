0dbfdd227d8dfc01f599a4ddb009b13c410639dc
Rafael Cortes <rafael_cortes@outlook.com>
2024-02-23 17:03:59 -0300

    G7 - Trabalhando com Json

--- 03-gerenciamento-usuarios/index.js
+++ 03-gerenciamento-usuarios/index.js
@@ -1,18 +1,16 @@
-// var name = document.querySelector('#exampleInputName');
-// var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
-// var birth = document.querySelector('#exampleInputBirth');
-// var country = document.querySelector('#exampleInputCountry');
-// var email = document.querySelector('#exampleInputEmail');
-// var password = document.querySelector('#exampleInputPassword');
-// var photo = document.querySelector('#exampleInputFile');
-// var admin = document.querySelector('#exampleInputAdmin');
-
+// @ts-nocheck
 var fields = document.querySelectorAll('#form-user-create [name]');
+
+var user = {};
+
 fields.forEach(function (field, index) {
     if (field.name == 'gender') {
-        if (field.checked) console.log('Sim', field);
+        if (field.checked) {
+            user[field.name] = field.value;
+        }
     } else {
-        console.log('Não');
+        user[field.name] = field.value;
     }
-    // console.log(field.id, field.name, field.value, field.checked, index);
 });
+
+console.log(user);
