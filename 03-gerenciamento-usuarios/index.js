4e77ff68261a5560c94771daf7cb19e6847845a3
Rafael Cortes <rafael_cortes@outlook.com>
2024-02-23 15:34:13 -0300

    G5 - For Each

--- 03-gerenciamento-usuarios/index.js
+++ 03-gerenciamento-usuarios/index.js
@@ -1,9 +1,14 @@
-// @ts-ignore
-var name = document.querySelector('#exampleInputName');
-var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
-var birth = document.querySelector('#exampleInputBirth');
-var country = document.querySelector('#exampleInputCountry');
-var email = document.querySelector('#exampleInputEmail');
-var password = document.querySelector('#exampleInputPassword');
-var photo = document.querySelector('#exampleInputFile');
-var admin = document.querySelector('#exampleInputAdmin');
+// var name = document.querySelector('#exampleInputName');
+// var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
+// var birth = document.querySelector('#exampleInputBirth');
+// var country = document.querySelector('#exampleInputCountry');
+// var email = document.querySelector('#exampleInputEmail');
+// var password = document.querySelector('#exampleInputPassword');
+// var photo = document.querySelector('#exampleInputFile');
+// var admin = document.querySelector('#exampleInputAdmin');
+
+var fields = document.querySelectorAll('#form-user-create [name]');
+fields.forEach(function (field, index) {
+    // @ts-ignore
+    console.log(field.id, field.name, field.value, field.checked, index);
+});
