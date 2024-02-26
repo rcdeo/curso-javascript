743e1736ee11ab13b26a43b8b3b7da1c64ff3e21
Rafael Cortes <rafael_cortes@outlook.com>
2024-02-26 15:49:16 -0300

    G11 - POO - MVC - Controller

--- 03-gerenciamento-usuarios/models/User.js
+++ 03-gerenciamento-usuarios/models/User.js
@@ -1,12 +1,13 @@
 class User {
+    // prettier-ignore
     constructor(name, gender, birth, country, email, password, photo, admin) {
-        this.name = name;
-        this.gender = gender;
-        this.birth = birth;
-        this.country = country;
-        this.email = email;
+        this.name     = name;
+        this.gender   = gender;
+        this.birth    = birth;
+        this.country  = country;
+        this.email    = email;
         this.password = password;
-        this.photo = photo;
-        this.admin = admin;
+        this.photo    = photo;
+        this.admin    = admin;
     }
 }
