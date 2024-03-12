3e3dc20d755c4ee142a88fc94ab078268fdbefd5
Rafael Cortes <rafael_cortes@outlook.com>
2024-03-12 18:33:18 -0300

    N08 - Carregando Rotas com Consign

--- 04-nodejs/routes/users.js
+++ 04-nodejs/routes/users.js
@@ -1,26 +1,23 @@
-let express = require('express');
-let routes = express.Router();
-
-routes.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: [
-            {
-                name: 'Fulano',
-                email: 'fulano@example.com',
-                id: 1,
-            },
-        ],
+module.exports = (app) => {
+    app.get('/users', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [
+                {
+                    name: 'Fulano',
+                    email: 'fulano@example.com',
+                    id: 1,
+                },
+            ],
+        });
     });
-});
 
-routes.get('/admin', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: [],
+    app.get('/users/admin', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [],
+        });
     });
-});
-
-module.exports = routes;
+};
