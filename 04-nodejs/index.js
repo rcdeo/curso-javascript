9bcce6fd8f3ee568af3d3ba72cd1c1c63a8d55c0
Rafael Cortes <rafael_cortes@outlook.com>
2024-03-12 12:10:09 -0300

    N06 - Nodemon e criando o servidor com Express

--- 04-nodejs/index.js
+++ 04-nodejs/index.js
@@ -1,33 +1,27 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
+let app = express();
 
-    switch (req.url) {
-        case '/':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Olá</h1>');
-            break;
-        case '/users':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(
-                JSON.stringify({
-                    users: [
-                        {
-                            name: 'Fulano',
-                            email: 'fulano@example.com',
-                            id: 1,
-                        },
-                    ],
-                })
-            );
-            break;
-    }
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello!</h1>');
 });
 
-server.listen(3000, '127.0.0.1', () => {
-    console.log('server running...');
+app.get('/users', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users: [
+            {
+                name: 'Fulano',
+                email: 'fulano@example.com',
+                id: 1,
+            },
+        ],
+    });
+});
+
+app.listen(3000, '127.0.0.1', () => {
+    console.log('Server Running...');
 });
