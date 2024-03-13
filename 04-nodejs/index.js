4223fd0936f097ffe2bfafc49ae4ede905a3e550
Rafael Cortes <rafael_cortes@outlook.com>
2024-03-13 12:05:37 -0300

    N09 - Recebendo dados via POST e instalando o Postman

--- 04-nodejs/index.js
+++ 04-nodejs/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 let app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// @ts-ignore
 consign().include('routes').into(app);
 
 app.listen(3000, '127.0.0.1', () => {
