c1eedcc3981e9a763d989c821265db161f82b77f
Rafael Cortes <rafael_cortes@outlook.com>
2024-03-19 22:42:15 -0300

    D08 - Adicionar o Firebase a sua Aplicação Web

--- 06-projeto-dropbox/app/public/src/controller/DropboxController.js
+++ 06-projeto-dropbox/app/public/src/controller/DropboxController.js
@@ -8,7 +8,21 @@ class DropboxController {
         this.filenameEl = this.snackModalEl.querySelector('.filename');
         this.timeleftEl = this.snackModalEl.querySelector('.timeleft');
 
+        this.connectFirebase();
         this.initEvents();
+    }
+
+    connectFirebase() {
+        const config = {
+            apiKey: 'YOUR_API_KEY',
+            authDomain: 'YOUR_AUTH_DOMAIN',
+            databaseURL: 'YOUR_DATABASE_URL',
+            projectId: 'YOUR_PROJECT_ID',
+            storageBucket: 'YOUR_STORAGE_BUCKET',
+            messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+            appId: 'YOUR_APP_ID',
+        };
+        firebase.initializeApp(config);
     }
 
     initEvents() {
