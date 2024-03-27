05d48e1cf08ad78aef523e37707250aa2e766306
Rafael Cortes <rafael_cortes@outlook.com>
2024-03-27 15:02:05 -0300

    D21 - Excluindo arquivo do Firebase Storage

--- 06-projeto-dropbox/app/public/src/controller/DropboxController.js
+++ 06-projeto-dropbox/app/public/src/controller/DropboxController.js
@@ -47,11 +47,23 @@ class DropboxController {
             let file = JSON.parse(li.dataset.file);
             let key = li.dataset.key;
 
-            let formData = new FormData();
-            formData.append('path', file.path);
-            formData.append('key', key);
-
-            promises.push(this.ajax('/file', 'DELETE', formData));
+            promises.push(
+                new Promise((resolve, reject) => {
+                    let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.name);
+                    fileRef
+                        .delete()
+                        .then((result) => {
+                            resolve({
+                                fields: {
+                                    key,
+                                },
+                            });
+                        })
+                        .catch((err) => {
+                            reject(err);
+                        });
+                })
+            );
         });
         return Promise.all(promises);
     }
