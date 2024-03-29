11d73d571f8a32ed1b751680c266d31addfb5064
Rafael Cortes <rafael_cortes@outlook.com>
2024-03-29 16:56:37 -0300

    W05 - Obtendo dados dos painéis e usando FormData

--- 07-projeto-whatsapp/src/controller/WhatsAppController.js
+++ 07-projeto-whatsapp/src/controller/WhatsAppController.js
@@ -27,6 +27,7 @@ class WhatsAppController {
      *   * Eventos: on(events, fn)
      *   * Estilos: css(styles)
      *   * Classes: addClass(name), removeClass(name), toggleClass(name), hasClass(name)
+     *   * Formulários: getForm(), toJSON()
      */
     elementsPrototype() {
         Element.prototype.hide = function () {
@@ -76,6 +77,18 @@ class WhatsAppController {
         Element.prototype.hasClass = function (name) {
             return this.classList.contains(name);
         };
+
+        HTMLFormElement.prototype.getForm = function () {
+            return new FormData(this);
+        };
+
+        HTMLFormElement.prototype.toJSON = function () {
+            let json = {};
+            this.getForm().forEach((value, key) => {
+                json[key] = value;
+            });
+            return json;
+        };
     }
 
     initEvents() {
@@ -101,6 +114,27 @@ class WhatsAppController {
 
         this.el.btnClosePanelAddContact.on('click', (e) => {
             this.el.panelAddContact.removeClass('open');
+        });
+
+        this.el.photoContainerEditProfile.on('click', (e) => {
+            this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', (e) => {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', (e) => {
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+        });
+
+        this.el.formPanelAddContact.on('submit', (e) => {
+            e.preventDefault();
+
+            let formData = new FormData(this.el.formPanelAddContact);
         });
     }
 
