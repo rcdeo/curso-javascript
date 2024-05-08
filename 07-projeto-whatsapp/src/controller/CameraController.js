8f3e38c4759955e75c2f3a51dd0a20fabf41b6c2
Rafael Cortes <rafael_cortes@outlook.com>
2024-05-08 12:04:32 -0300

    W17 - Parando de capturar imagem da câmera - getTracks()

--- 07-projeto-whatsapp/src/controller/CameraController.js
+++ 07-projeto-whatsapp/src/controller/CameraController.js
@@ -7,11 +7,18 @@ export class CameraController {
                 video: true,
             })
             .then((stream) => {
+                this._stream = stream;
                 this._videoEl.srcObject = new MediaStream(stream);
                 this._videoEl.play();
             })
             .catch((err) => {
                 console.error(err);
             });
+    }
+
+    stop() {
+        this._stream.getTracks().forEach((track) => {
+            track.stop();
+        });
     }
 }
