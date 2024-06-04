0fa4fbc4bf9a96c8afd5a45d7780f2ed93468315
Rafael Cortes <rafael_cortes@outlook.com>
2024-06-04 16:45:48 -0300

    MY11 - Efetuando Reserva com Método POST para o MySQL

--- 08-projeto-restaurante/saboroso/routes/index.js
+++ 08-projeto-restaurante/saboroso/routes/index.js
@@ -1,6 +1,7 @@
 var conn = require('./../inc/db');
 var express = require('express');
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 var router = express.Router();
 
 /* GET home page. */
@@ -37,11 +38,32 @@ router.get('/menus', function (req, res, next) {
 
 /* GET reservations page. */
 router.get('/reservations', function (req, res, next) {
-    res.render('reservations', {
-        title: 'Reserva - Restaurante Saboroso!',
-        background: 'images/img_bg_2.jpg',
-        h1: 'Reserve uma mesa!',
-    });
+    reservations.render(req, res);
+});
+
+/* POST reservations form. */
+router.post('/reservations', function (req, res, next) {
+    if (!req.body.name) {
+        reservations.render(req, res, 'Digite o seu nome.');
+    } else if (!req.body.email) {
+        reservations.render(req, res, 'Digite o seu e-mail.');
+    } else if (!req.body.people) {
+        reservations.render(req, res, 'Selecione o nro. de pessoas.');
+    } else if (!req.body.date) {
+        reservations.render(req, res, 'Selecione a data da reserva.');
+    } else if (!req.body.time) {
+        reservations.render(req, res, 'Selecione a hora da reserva.');
+    } else {
+        reservations
+            .save(req.body)
+            .then((results) => {
+                req.body = {};
+                reservations.render(req, res, null, 'Reserva efetuada com sucesso!');
+            })
+            .catch((err) => {
+                reservations.render(req, res, err.message);
+            });
+    }
 });
 
 /* GET services page. */
